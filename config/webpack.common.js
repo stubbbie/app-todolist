6ce980475a317caa99b9d0878c32ae28a8066241
Wilden <steffen.wilden@lorien.fkie.fgan.de>
2017-05-19 16:17:50 +0200

    first commit

---
+++ config/webpack.common.js
@@ -0,0 +1,99 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [helpers.root('node_modules')],
+    alias: {
+          'api': helpers.root('api/server')
+    }
+  },
+
+  externals: [
+                resolveExternals
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('src', 'tsconfig.json') }
+          } , 'angular2-template-loader'
+        ]
+      },
+
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
+      },
+
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+   new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src'),
+      {}
+    ),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
+
+function resolveExternals(context, request, callback) 
+{
+  return resolveMeteor(request, callback) ||
+  callback();
+}
+
+function resolveMeteor(request, callback) 
+{
+  var match = request.match(/^meteor\/(.+)$/);
+  var pack = match && match[1];
+  if (pack) {
+    callback(null, 'Package["' + pack + '"]');
+    return true;
+  }
+
+}
